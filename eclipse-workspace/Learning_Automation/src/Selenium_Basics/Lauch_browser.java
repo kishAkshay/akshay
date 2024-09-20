dd6fc97084848bab3b747315dc6e35b114bdcda6
Akshay M <akshay.mandekar@glance.com>
2024-09-20 15:08:07 +0530

    Adding the eclipse workspace folder

---
+++ eclipse-workspace/Learning_Automation/src/Selenium_Basics/Lauch_browser.java
@@ -0,0 +1,26 @@
+package Selenium_Basics;
+
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Lauch_browser {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/");
+		@Nullable
+		String TitleOfPage = driver.getTitle();
+		System.out.println(TitleOfPage);
+		@Nullable
+		String CurrenturlOfPage = driver.getCurrentUrl();
+		System.out.println(CurrenturlOfPage);
+		Thread.sleep(2000);
+		//driver.close();
+		driver.quit();
+		
+	}
+
+}
