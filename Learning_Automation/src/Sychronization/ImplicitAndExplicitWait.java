bc40214829d43f145be0d7f0c2d4c0ae79b4af52
TYSS-AKSHAY\Akshay <akshaymandekar61@gmail.com>
2024-09-29 23:48:36 +0530

    no message

--- Learning_Automation/src/Sychronization/ImplicitAndExplicitWait.java
+++ Learning_Automation/src/Sychronization/ImplicitAndExplicitWait.java
@@ -1,5 +1,6 @@
 package Selenium_Basics;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -8,23 +9,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class AddMultipleProductToCart {
+public class ImplicitAndExplicitWait {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		WebDriverWait w = new WebDriverWait(driver,Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 		String[] itemsNeeded = { "Brocolli", "Cucumber", "Beetroot", "Capsicum", "Banana" };
 
 		selectItems(driver, itemsNeeded);
 		driver.findElement(By.xpath("//img[@alt='Cart']")).click();
 		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
-		Thread.sleep(2000);
+		//Explicit Wait
+		w.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//input[@class='promoCode']")));
 		driver.findElement(By.xpath("//input[@class='promoCode']")).sendKeys("rahulshettyacademy");
 		driver.findElement(By.xpath("//button[@class='promoBtn']")).click();
-		Thread.sleep(5000);
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='promoInfo']")));
 		String promoInfo = driver.findElement(By.xpath("//span[@class='promoInfo']")).getText();
 		System.out.println(promoInfo);
 
@@ -61,6 +67,8 @@ public class AddMultipleProductToCart {
 			}
 
 		}
+
+
 	}
 
 }
